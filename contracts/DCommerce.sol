4426595ebc1e06d3a0dbd977de368b5c4ef7f65f
Petar Popovic <petarpnp@gmail.com>
2021-11-24 23:56:21 +0100

    Make DCommerce derivative of ERC1155

--- contracts/DCommerce.sol
+++ contracts/DCommerce.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.10;
 
-import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/presets/ERC1155PresetMinterPauser.sol";
 
-contract DCommerce is ERC1155 {
-  constructor(string memory _baseURL) ERC1155(_baseURL) {
+contract DCommerce is ERC1155PresetMinterPauser {
+  constructor(string memory _baseURL) ERC1155PresetMinterPauser(_baseURL) {
   }
 }
