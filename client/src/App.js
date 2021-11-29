2b346fe99b6edc8f6cffab07fb7c76bca40d6255
Petar Popovic <petarpnp@gmail.com>
2021-11-29 23:45:26 +0100

    Initialize client app

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Web3ReactProvider } from "@web3-react/core";
+import { ethers } from "ethers";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Route } from "react-router-dom";
+import "./styles/App.css";
+import Home from "./pages/Home";
+import Header from "./components/Header";
+
+function getLibrary(provider) {
+  return new ethers.providers.Web3Provider(provider);
+}
+
+const App = () => {
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <div>
+        <Header />
+        <Route exact path="/" component={Home} />
+      </div>
+    </Web3ReactProvider>
+  );
+};
+
+export default App;
---
+++ client/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MetamaskConnectButton from "./MetamaskConnectButton";
+
+const Header = () => {
+  return (
+    <nav>
+      <MetamaskConnectButton />
+    </nav>
+  );
+};
+
+export default Header;
