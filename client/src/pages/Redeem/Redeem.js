53b300bbf0e0e8637c11fe28486fba1ab1f13214
Petar Popovic <petarpnp@gmail.com>
2021-12-05 00:22:53 +0100

    Add approval step to Redeem page

--- client/src/pages/Redeem/Redeem.js
+++ client/src/pages/Redeem/Redeem.js
@@ -7,6 +7,7 @@ import styles from "./Redeem.module.scss";
 import {useContract} from "../../hooks/useContract";
 import {StoreContract} from "../../static/contracts/StoreContract";
 import {DCommerceContract} from "../../static/contracts/DCommerceContract";
+import {toast} from "react-toastify";
 
 const Redeem = () => {
   const params = useParams();
@@ -14,6 +15,9 @@ const Redeem = () => {
   const [ isPageLoading, setIsPageLoading ] = useState(true);
   const [ product, setProduct ] = useState(true);
   const [ copiesOwnedByUser, setCopiesOwnedByUser ] = useState(0);
+  const [ isApproved, setIsApproved ] = useState(false);
+  const [ waitingForTransaction, setWaitingForTransaction ] = useState(false);
+  const [ waitingForRedeemTransaction, setWaitingForRedeemTransaction ] = useState(false);
   const StoreContractInstance = useContract(StoreContract.address[chainId], StoreContract.abi);
   const DCommerceContractInstance = useContract(DCommerceContract.address[chainId], DCommerceContract.abi);
 
@@ -30,7 +34,9 @@ const Redeem = () => {
       priceEther
     }
     const userBalance = await DCommerceContractInstance.balanceOf(account, productId);
+    const isApprovedAlready = await DCommerceContractInstance.isApprovedForAll(account, StoreContractInstance.address);
 
+    setIsApproved(isApprovedAlready)
     setProduct(productData);
     setCopiesOwnedByUser(userBalance.toNumber());
     setIsPageLoading(false);
@@ -41,6 +47,26 @@ const Redeem = () => {
       fetchPageData();
     }
   }, [StoreContractInstance]);
+
+  const setStoreAsOperator = async () => {
+    try {
+      setWaitingForTransaction(true);
+      const transaction = await DCommerceContractInstance.setApprovalForAll(StoreContractInstance.address, true);
+      await transaction.wait(1);
+      const isApproved = await DCommerceContractInstance.isApprovedForAll(account, StoreContractInstance.address);
+
+      setWaitingForTransaction(false);
+      toast.success("Approval successful", {position: "bottom-right"});
+      setIsApproved(isApproved);
+    } catch (e) {
+      setWaitingForTransaction(false);
+      toast.error(e.message, {position: "bottom-right"});
+    }
+  };
+
+  const redeemItem = () => console.log("redeem");
+
+  const onButtonClick = isApproved ? redeemItem : setStoreAsOperator;
 
   if (isPageLoading) {
     return <div className={styles.wrapperFlex}>Fetching product....</div>;
@@ -57,8 +83,12 @@ const Redeem = () => {
       <img src={product.image} alt="Product image" />
       <p className={styles.productName}>{product.name}</p>
       <p className={styles.productDescription}>{product.description}</p>
-      <p className={styles.disclaimer}>You own {copiesOwnedByUser} out of {product.stock} copies. Would you like to redeem one now?</p>
-      <button className={styles.redeemButton}>Redeem</button>
+      <p className={styles.disclaimer}>{
+        isApproved ?
+        `You own ${copiesOwnedByUser} out of ${product.stock} copies. Would you like to redeem one now?` :
+        `Before you can redeem you have to approve. You have to do this only once`
+      }</p>
+      <button onClick={onButtonClick} className={styles.redeemButton}>{isApproved ? (waitingForRedeemTransaction ? "Redeeming..." : "Redeem") : (waitingForTransaction ? "Approving..." : "Approve")}</button>
     </div>
   )
 };
