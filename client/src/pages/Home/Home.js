0251836cacf8f7df336900fbfe6f5f240a3dddbd
Petar Popovic <petarpnp@gmail.com>
2021-12-05 00:24:11 +0100

    Run prettier

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,7 +1,7 @@
-import React, {useCallback, useEffect, useState} from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useWeb3React } from "@web3-react/core";
 import { ethers } from "ethers";
-import {toast} from "react-toastify";
+import { toast } from "react-toastify";
 import { Link } from "react-router-dom";
 import styles from "./Home.module.scss";
 import { useContract } from "../../hooks/useContract";
@@ -9,8 +9,8 @@ import { StoreContract } from "../../static/contracts/StoreContract";
 
 const Home = () => {
   const { chainId } = useWeb3React();
-  const [ isPageLoading, setIsPageLoading ] = useState(true);
-  const [ productList, setProductList ] = useState([]);
+  const [isPageLoading, setIsPageLoading] = useState(true);
+  const [productList, setProductList] = useState([]);
   const StoreContractInstance = useContract(StoreContract.address[chainId], StoreContract.abi);
 
   const fetchPageData = useCallback(async () => {
@@ -25,11 +25,11 @@ const Home = () => {
         id: i,
         ...metadata,
         priceInWei,
-        priceEther
-      }
+        priceEther,
+      };
       products.push(productData);
     }
-    setProductList(products)
+    setProductList(products);
     setIsPageLoading(false);
   }, [StoreContractInstance]);
 
@@ -41,13 +41,13 @@ const Home = () => {
 
   const handleBuyClick = async (id, priceInWei) => {
     try {
-      const transaction = await StoreContractInstance.buyItems(id, 1, {value: priceInWei});
+      const transaction = await StoreContractInstance.buyItems(id, 1, { value: priceInWei });
       await transaction.wait(1);
-      toast.success("Purchase successful", {position: "bottom-right"});
+      toast.success("Purchase successful", { position: "bottom-right" });
     } catch (e) {
-      toast.error(e.message, {position: "bottom-right"});
+      toast.error(e.message, { position: "bottom-right" });
     }
-  }
+  };
 
   if (isPageLoading) {
     return <div className={styles.wrapperFlex}>Fetching products....</div>;
@@ -58,26 +58,26 @@ const Home = () => {
       <div className={styles.wrapperFlex}>
         <p>No products added. Go to Admin page and add some.</p>
       </div>
-    )
+    );
   }
 
   return (
     <div className={styles.container}>
       <div className={styles.productsGrid}>
-        {
-          productList.map((product, i) => (
-            <div key={i}>
-              <img src={product.image} alt="Product image" />
-              <p className={styles.productName}>{product.name}</p>
-              <p className={styles.productDescription}>{product.description}</p>
-              <p className={styles.productPrice}>{product.priceEther} ETH</p>
-              <button className={styles.buyButton} onClick={() => handleBuyClick(product.id, product.priceInWei)}>Buy item</button>
-              <Link to={`/redeem/${product.id}`}>
-                <button className={styles.redeemButton}>Redeem item</button>
-              </Link>
-            </div>
-          ))
-        }
+        {productList.map((product, i) => (
+          <div key={i}>
+            <img src={product.image} alt="Product image" />
+            <p className={styles.productName}>{product.name}</p>
+            <p className={styles.productDescription}>{product.description}</p>
+            <p className={styles.productPrice}>{product.priceEther} ETH</p>
+            <button className={styles.buyButton} onClick={() => handleBuyClick(product.id, product.priceInWei)}>
+              Buy item
+            </button>
+            <Link to={`/redeem/${product.id}`}>
+              <button className={styles.redeemButton}>Redeem item</button>
+            </Link>
+          </div>
+        ))}
       </div>
     </div>
   );
--- client/src/pages/Redeem/Redeem.js
+++ client/src/pages/Redeem/Redeem.js
@@ -1,23 +1,23 @@
-import React, {useCallback, useEffect, useState} from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import {useWeb3React} from "@web3-react/core";
-import {ethers} from "ethers";
+import { useWeb3React } from "@web3-react/core";
+import { ethers } from "ethers";
 
 import styles from "./Redeem.module.scss";
-import {useContract} from "../../hooks/useContract";
-import {StoreContract} from "../../static/contracts/StoreContract";
-import {DCommerceContract} from "../../static/contracts/DCommerceContract";
-import {toast} from "react-toastify";
+import { useContract } from "../../hooks/useContract";
+import { StoreContract } from "../../static/contracts/StoreContract";
+import { DCommerceContract } from "../../static/contracts/DCommerceContract";
+import { toast } from "react-toastify";
 
 const Redeem = () => {
   const params = useParams();
   const { account, chainId } = useWeb3React();
-  const [ isPageLoading, setIsPageLoading ] = useState(true);
-  const [ product, setProduct ] = useState(true);
-  const [ copiesOwnedByUser, setCopiesOwnedByUser ] = useState(0);
-  const [ isApproved, setIsApproved ] = useState(false);
-  const [ waitingForTransaction, setWaitingForTransaction ] = useState(false);
-  const [ waitingForRedeemTransaction, setWaitingForRedeemTransaction ] = useState(false);
+  const [isPageLoading, setIsPageLoading] = useState(true);
+  const [product, setProduct] = useState(true);
+  const [copiesOwnedByUser, setCopiesOwnedByUser] = useState(0);
+  const [isApproved, setIsApproved] = useState(false);
+  const [waitingForTransaction, setWaitingForTransaction] = useState(false);
+  const [waitingForRedeemTransaction, setWaitingForRedeemTransaction] = useState(false);
   const StoreContractInstance = useContract(StoreContract.address[chainId], StoreContract.abi);
   const DCommerceContractInstance = useContract(DCommerceContract.address[chainId], DCommerceContract.abi);
 
@@ -31,12 +31,12 @@ const Redeem = () => {
       id: productId,
       ...metadata,
       priceInWei,
-      priceEther
-    }
+      priceEther,
+    };
     const userBalance = await DCommerceContractInstance.balanceOf(account, productId);
     const isApprovedAlready = await DCommerceContractInstance.isApprovedForAll(account, StoreContractInstance.address);
 
-    setIsApproved(isApprovedAlready)
+    setIsApproved(isApprovedAlready);
     setProduct(productData);
     setCopiesOwnedByUser(userBalance.toNumber());
     setIsPageLoading(false);
@@ -56,11 +56,11 @@ const Redeem = () => {
       const isApproved = await DCommerceContractInstance.isApprovedForAll(account, StoreContractInstance.address);
 
       setWaitingForTransaction(false);
-      toast.success("Approval successful", {position: "bottom-right"});
+      toast.success("Approval successful", { position: "bottom-right" });
       setIsApproved(isApproved);
     } catch (e) {
       setWaitingForTransaction(false);
-      toast.error(e.message, {position: "bottom-right"});
+      toast.error(e.message, { position: "bottom-right" });
     }
   };
 
@@ -74,7 +74,11 @@ const Redeem = () => {
 
   if (!copiesOwnedByUser) {
     return (
-      <div className={styles.wrapperFlex}>You don't own any copy of this product so there's nothing to redeem.<br />Go to home page and buy one.</div>
+      <div className={styles.wrapperFlex}>
+        You don't own any copy of this product so there's nothing to redeem.
+        <br />
+        Go to home page and buy one.
+      </div>
     );
   }
 
@@ -83,14 +87,22 @@ const Redeem = () => {
       <img src={product.image} alt="Product image" />
       <p className={styles.productName}>{product.name}</p>
       <p className={styles.productDescription}>{product.description}</p>
-      <p className={styles.disclaimer}>{
-        isApproved ?
-        `You own ${copiesOwnedByUser} out of ${product.stock} copies. Would you like to redeem one now?` :
-        `Before you can redeem you have to approve. You have to do this only once`
-      }</p>
-      <button onClick={onButtonClick} className={styles.redeemButton}>{isApproved ? (waitingForRedeemTransaction ? "Redeeming..." : "Redeem") : (waitingForTransaction ? "Approving..." : "Approve")}</button>
+      <p className={styles.disclaimer}>
+        {isApproved
+          ? `You own ${copiesOwnedByUser} out of ${product.stock} copies. Would you like to redeem one now?`
+          : `Before you can redeem you have to approve. You have to do this only once`}
+      </p>
+      <button onClick={onButtonClick} className={styles.redeemButton}>
+        {isApproved
+          ? waitingForRedeemTransaction
+            ? "Redeeming..."
+            : "Redeem"
+          : waitingForTransaction
+          ? "Approving..."
+          : "Approve"}
+      </button>
     </div>
-  )
+  );
 };
 
 export default Redeem;
